d281579afc666e6a85de0228df5b258175e592a3
Shivank Pathak <shivankpathak357@gmail.com>
2017-12-16 00:30:11 +0530

    Fix errors in convolution and pooling layer

--- convolution_neural_net.py
+++ convolution_neural_net.py
@@ -18,8 +18,6 @@ def maxpool2d(x):
     #                        size of window         movement of window
     return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 
-
-
 def convolutional_neural_network(x):
     weights = {'W_conv1':tf.Variable(tf.random_normal([5,5,1,32])),
                'W_conv2':tf.Variable(tf.random_normal([5,5,32,64])),
@@ -35,13 +33,12 @@ def convolutional_neural_network(x):
 
     conv1 = tf.nn.relu(conv2d(x, weights['W_conv1']) + biases['b_conv1'])
     conv1 = maxpool2d(conv1)
-    
+
     conv2 = tf.nn.relu(conv2d(conv1, weights['W_conv2']) + biases['b_conv2'])
     conv2 = maxpool2d(conv2)
 
     fc = tf.reshape(conv2,[-1, 7*7*64])
     fc = tf.nn.relu(tf.matmul(fc, weights['W_fc'])+biases['b_fc'])
-    
     fc = tf.nn.dropout(fc, keep_rate)
 
     output = tf.matmul(fc, weights['out'])+biases['out']
@@ -55,7 +52,7 @@ def train_neural_network(x):
     
     hm_epochs = 10
     with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+        sess.run(tf.initialize_all_variables())
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
@@ -64,7 +61,7 @@ def train_neural_network(x):
                 _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
 
-            print('Epoch', epoch+1, 'completed out of',hm_epochs,'loss:',epoch_loss)
+            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
 
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
 

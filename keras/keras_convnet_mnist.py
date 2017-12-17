62ec9960bf81762608f8f92e278c745deab8cc39
Shivank Pathak <shivankpathak357@gmail.com>
2017-12-18 00:50:14 +0530

    Implement convolution neural network on mnist dataset using keras

---
+++ keras/keras_convnet_mnist.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+import keras
+from keras.datasets import mnist
+from keras.layers import Input, Dense, Conv2D,MaxPooling2D,Flatten,Dropout
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras import backend as K
+from keras.losses import categorical_crossentropy
+import numpy as np
+
+(x_train,y_train),(x_test,y_test)=mnist.load_data()
+
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+img_rows, img_cols = 28, 28
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+y_train = to_categorical(y_train, num_classes)
+y_test = to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape),)
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.8))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+model.compile(loss=categorical_crossentropy,
+              optimizer=keras.optimizers.Adam(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
